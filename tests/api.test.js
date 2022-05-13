a3746bcc7fff17b642181cceff35820721771b94
WilliamDaniels <47723491+William-Daniels1995@users.noreply.github.com>
2022-05-13 16:04:27 +0100

    Added Testing with Jest
    Tests the GET branches endpoint.

---
+++ tests/api.test.js
@@ -0,0 +1,38 @@
+const request = require( 'supertest' )
+const app = require( '../app' )
+
+describe( 'API tests', () => {
+  describe( 'GET /branches', () => {
+
+    const route = '/api/v1/branches'
+
+    test( 'Invalid Branch', async () => {
+      const response = await request( app ).get( route )
+      expect( response.statusCode ).toBe( 422 )
+    })
+
+    test( 'Unknown Branch', async () => {
+      const response = await request( app ).get( route ).set( 'lbg-txn-branch-location', 'Planet Jupiter' )
+      expect( response.statusCode ).toBe( 404 )
+    })
+
+    test( 'Valid Branch', async () => {
+      const branch = 'London'
+      const response = await request( app ).get( route ).set( 'lbg-txn-branch-location', branch )
+      expect( response.statusCode ).toBe( 200 )
+
+      const type = response.headers[ 'content-type' ]
+      expect( type ).toMatch( /json/ )
+      expect( response.body ).toBeInstanceOf( Array )
+
+      response.body.forEach( item => {
+        expect( item ).toHaveProperty( 'Identification' )
+        expect( item ).toHaveProperty( 'SequenceNumber' )
+        expect( item ).toHaveProperty( 'Name' )
+        expect( item ).toHaveProperty( 'Type' )
+        expect( item ).toHaveProperty( 'Type' )
+        expect( item?.PostalAddress?.TownName ).toBe( branch.toUpperCase() )
+      })
+    })
+  })
+})
