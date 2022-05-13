ffa5e640741c9c9d63532a73d174c1f2008e3e3e
WilliamDaniels <47723491+William-Daniels1995@users.noreply.github.com>
2022-05-13 21:27:49 +0100

    Added comments

--- api/routes/branches.js
+++ api/routes/branches.js
@@ -3,14 +3,17 @@ const axios = require( 'axios' )
 module.exports = {
   get: async ( req, res, next ) => {
     //Request the data and deconstruct
+    const branch = req.headers[ 'lbg-txn-branch-location' ]
+    console.log( `Requesting branches in ${ branch }` )
+
     const route = 'https://api.lloydsbank.com/open-banking/v2.2/branches'
     const response = await axios.get( route ).catch( console.log )
     const data = response?.data?.data?.[ 0 ]?.Brand?.[ 0 ]
     if ( !data ) return res.sendStatus( 504 )
 
     //Filter based on branch location
-    const query = req.headers[ 'lbg-txn-branch-location' ]
-    const branches = data.Branch.filter( o => o?.PostalAddress?.TownName === query.toUpperCase() )
+    const branches = data.Branch.filter( o => o?.PostalAddress?.TownName === branch.toUpperCase() )
+    console.log( `Found ${ branches.length } branches` )
     if ( !branches.length ) return res.sendStatus( 404 )
     return res.status( 200 ).send( branches )
   }
